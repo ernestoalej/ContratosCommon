d367158af4e6baddec5e1cc132a865e1014f6902
Ernesto Contreras <ernesto.contreras@aureustech>
2025-08-31 23:29:25 -0400

    agregar comentario al record de activo

--- ContractosCommon.Contracts/Activo.cs
+++ ContractosCommon.Contracts/Activo.cs
@@ -2,6 +2,7 @@
 {
 	public record Activo
 	{
+		// clase pasra exponer los activos de expediente
 		public int Id { get; set; }
 		public required string Nombre { get; set; }		
 		public required string CodActivo { get; set; }
