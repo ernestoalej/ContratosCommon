b07646fe3a8089c46d7e9082a2da2d502212789f
Ernesto Contreras <ernesto.contreras@aureustech>
2025-08-31 22:58:56 -0400

    agregar nueva propiedad al contrato desde  el submodulo

--- ContractosCommon.Contracts/Activo.cs
+++ ContractosCommon.Contracts/Activo.cs
@@ -1,8 +1,9 @@
 ﻿namespace ContractosCommon.Contracts
 {
-    public record Activo
-    {	
+	public record Activo
+	{
 		public int Id { get; set; }
-		public required string Nombre { get; set; }
+		public required string Nombre { get; set; }		
+		public required string CodActivo { get; set; }
 	}
 }
